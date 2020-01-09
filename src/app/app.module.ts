a7beea57db08fc33c5523351b97fe5b113f9ef34
sebastian <haa17482@spengergasse.at>
2020-01-09 11:30:50 +0100

    Abgabe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import {ExampleDialogComponent, AppointmentComponent, AddDialog, } from './appointment/appointment.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatButtonModule,
+  MatCardModule, MatDatepickerModule, MatDialogModule,
+  MatIconModule,
+  MatInputModule, MatNativeDateModule,
+  MatSortModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {MomentDateModule} from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    ExampleDialogComponent,
+    AddDialog,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatCardModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MomentDateModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ExampleDialogComponent, AddDialog, ]
 })
 export class AppModule { }
